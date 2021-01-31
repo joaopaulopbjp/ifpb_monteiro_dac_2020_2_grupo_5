2d3f7903c274ca59369915f77b0b2c86ae4f85c9
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-01-31 03:26:12 -0300

    Update Book finished

--- api/src/main/java/tk/bookzzz/api/resource/BookResource.java
+++ api/src/main/java/tk/bookzzz/api/resource/BookResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,7 +62,30 @@ public class BookResource {
   public ResponseEntity<Page<Book>> searchBooks(@PathVariable int pgNumber){
     return ResponseEntity.status(HttpStatus.OK).body(bookService.findBooks(pgNumber, 5));
   }
-  
+
+  @PutMapping(path = "/books/{id}")
+  public ResponseEntity<Book> update(@PathVariable long id, @RequestBody BookPostDTO book){
+    Book newBook = modelMapper.map(book, Book.class);
+    newBook.setAuthors(new ArrayList<Author>());
+    try {
+      Book helpBook = bookService.findById(id);
+      for(Author author: helpBook.getAuthors()){
+        author.getBooks().remove(helpBook);
+      }
+      for(Long authorId: book.getAuthors()){
+        Author author = authorService.findById(authorId);
+        author.getBooks().add(newBook);
+        newBook.getAuthors().add(author);
+      }
+      newBook.setId(id);
+      Book savedBook = bookService.save(newBook);
+      return ResponseEntity.status(HttpStatus.OK).body(savedBook);
+      
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Book());
+    }
+  }
+
   @DeleteMapping(path = "/books/{id}")
   public ResponseEntity<Book> deleteBook(@PathVariable long id){
     try {
